a894242f83dfb5974fc4b493481019fe75b99b37
coderAlsho <70171646+coderAlsho@users.noreply.github.com>
2022-05-14 09:56:01 -0700

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,19 @@
+
+
+
+class Atm:
+    def money():
+
+        obj = atmCardNumber
+        obj = pinNumber
+
+        atmCardNumber = input('enter your card number')
+        pinNumber = input('enter your pin number')
+
+        CashWithdrawl = -20
+        def Cashwithdrawl:
+            print(C)
+
+        BalenceEquity2 = +20
+        def BalenceEquity:
+            print(BalenceEquity2)
